6a9dbacc06618719ca9f8c795e5b29927ca96cde
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2023-05-07 16:26:55 +0300

    add cards

--- bot/handlers/handleInvokeAdditionalStepsByVerb.js
+++ bot/handlers/handleInvokeAdditionalStepsByVerb.js
@@ -18,6 +18,25 @@ async function handleInvokeAdditionalStepsByVerb(verb, data) {
     case "startPlan".toLowerCase() || "helpPlan".toLowerCase():
       await postSecondaryUserData(data);
       break;
+    case "ascReasonDecline".toLowerCase():
+      let whyNoPlan = data.value.action.data.reasonDecline;
+      if (whyNoPlan === "4") {
+        whyNoPlan = data.value.action.data.yourOptionReasonDecline;
+      }
+      const reasonWhyNoPlan = { reasonWhyNoPlan: `${whyNoPlan}` };
+      await postSecondaryUserData(data);
+      await postCustomUserData(reasonWhyNoPlan, userId);
+      break;
+    case "ascNotJoy".toLowerCase():
+      let whyNotJoyPlan = data.value.action.data.whyNotJoy;
+      if (whyNotJoyPlan === "4") {
+        whyNotJoyPlan = data.value.action.data.yourOptionWhyNotJoy;
+      }
+      const reasonWhyNotJoy = { reasonWhyNotJoy: `${whyNotJoyPlan}` };
+      console.log(reasonWhyNotJoy);
+      await postSecondaryUserData(data);
+      await postCustomUserData(reasonWhyNotJoy, userId);
+      break;
     default:
       return;
   }
