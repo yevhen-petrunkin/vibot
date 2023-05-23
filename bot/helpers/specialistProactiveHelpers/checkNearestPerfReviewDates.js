ea7982e9a9bbfe21856b77eb96b11be7c5b5ce76
yevhen-petrunkin <novamovaxxi@gmail.com>
2023-05-23 21:51:33 +0300

    Enable two more proactive cards

--- bot/helpers/specialistProactiveHelpers/checkNearestPerfReviewDates.js
+++ bot/helpers/specialistProactiveHelpers/checkNearestPerfReviewDates.js
@@ -1,29 +1,81 @@
 const checkIsDatePassed = require("../checkIsDatePassed");
 const getWeekEarlierDate = require("../getWeekEarlierDate");
 const fetchPerformanceReviewDates = require("../../db-functions/fetchPerformanceReviewDates");
+const updateUserByEmail = require("../../db-functions/updateUserByEmail");
 
 async function checkNearestPerfReviewDates(context, credentials) {
   const userName = context.activity.from.name;
-  try {
-    const dates = await fetchPerformanceReviewDates(credentials.companyName);
+  const { companyName, userPerfDates, userEmail } = credentials;
+  const { notifiedOnStart, notifiedOnEnd, perfStartDate } = userPerfDates;
+  const newDatesData = {
+    userPerfDates,
+  };
 
-    if (dates && dates.perfStartDate) {
-      if (checkIsDatePassed(dates.perfStartDate)) {
-        return null;
-      }
-      const weekEarlierDate = getWeekEarlierDate(dates.perfStartDate);
-      if (checkIsDatePassed(weekEarlierDate)) {
-        return {
-          userEmail: credentials.userEmail,
-          userName,
-          verb: "activeSpecRemainderPR",
-          triggerDate: weekEarlierDate,
-          data: dates,
-          complete: false,
-        };
-      }
+  try {
+    if (notifiedOnStart && !notifiedOnEnd) {
       return null;
     }
+
+    const dates = await fetchPerformanceReviewDates(companyName);
+
+    if (dates) {
+      if (dates.perfStartDate !== perfStartDate) {
+        newDatesData.userPerfDates.perfStartDate = await dates.perfStartDate;
+        newDatesData.userPerfDates.perfEndDate = await dates.perfEndDate;
+      }
+
+      if (!notifiedOnStart && !notifiedOnEnd) {
+        newDatesData.userPerfDates.notifiedOnStart = true;
+
+        if (checkIsDatePassed(dates.perfStartDate)) {
+          await updateUserByEmail(userEmail, newDatesData, {
+            context,
+            credentials,
+          });
+          return null;
+        }
+
+        const weekEarlierDate = getWeekEarlierDate(dates.perfStartDate);
+
+        if (checkIsDatePassed(weekEarlierDate)) {
+          await updateUserByEmail(userEmail, newDatesData, {
+            context,
+            credentials,
+          });
+          return {
+            userEmail,
+            userName,
+            verb: "activeSpecRemainderPR",
+            triggerDate: weekEarlierDate,
+            data: dates,
+            complete: false,
+          };
+        }
+
+        newDatesData.userPerfDates.notifiedOnStart = false;
+        await updateUserByEmail(userEmail, newDatesData, {
+          context,
+          credentials,
+        });
+
+        return null;
+      }
+
+      if (notifiedOnStart && notifiedOnEnd) {
+        if (dates.perfStartDate !== perfStartDate) {
+          newDatesData.userPerfDates.notifiedOnStart = false;
+          newDatesData.userPerfDates.notifiedOnEnd = false;
+          const temporaryCredentials = { ...credentials, ...newDatesData };
+          return await checkNearestPerfReviewDates(
+            context,
+            temporaryCredentials
+          );
+        }
+        return null;
+      }
+    }
+
+    return null;
   } catch (error) {
     console.log(error.message);
     console.log("checkNearestPerfReviewDates: Failed to create reminder.");
--- bot/sources/specProactiveCheckups.js
+++ bot/sources/specProactiveCheckups.js
@@ -1,9 +1,14 @@
 const checkNearestPerfReviewDates = require("../helpers/specialistProactiveHelpers/checkNearestPerfReviewDates");
+const checkPastPerfReviewDates = require("../helpers/specialistProactiveHelpers/checkPastPerfReviewDates");
 
 const specProactiveCheckups = [
   {
     verb: "activeSpecRemainderPR",
     checkup: checkNearestPerfReviewDates,
+  },
+  {
+    verb: "activeSpecRemainderStudy",
+    checkup: checkPastPerfReviewDates,
   },
 ];
 
